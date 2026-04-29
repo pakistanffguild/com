7242d0cee9ac30028cc06c1990fce225cfa9cb05
pakistanffguild <pakistanffguild@gmail.com>
2026-04-29 11:22:26 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,33 @@
 
+const CACHE_NAME = 'pff-guild-v1';
+const urlsToCache = [
+  '/com/',
+  '/com/index.html',
+  '/com/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
